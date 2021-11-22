d4232ada67cab561c96f4a0db11cbb4b7331eecd
Paulo Graça <paulo1978@gmail.com>
2021-11-22 12:42:27 +0000

    adding authorprofile creation sql

---
+++ src/main/resources/org/dspace/storage/rdbms/sqlmigration/h2/V5.0_2017.07.07__AuthorProfile_schema.sql
@@ -0,0 +1,16 @@
+-- ---------------
+-- AUTHOR PROFILES
+-- ---------------
+
+CREATE SEQUENCE authorprofile_seq;
+
+
+CREATE TABLE authorprofile
+(
+    authorprofile_id          INTEGER PRIMARY KEY,
+    photo_bitstream_id    INTEGER REFERENCES Bitstream(bitstream_id),
+    uuid            VARCHAR(50) NULL,
+    last_modified   TIMESTAMP
+    
+);
+CREATE INDEX authorprofile_photo_bitstream_fk_idx ON authorprofile(photo_bitstream_id);
