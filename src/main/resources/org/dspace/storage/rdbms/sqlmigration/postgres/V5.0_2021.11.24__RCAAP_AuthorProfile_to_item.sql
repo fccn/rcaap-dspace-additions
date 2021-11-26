ffa84d3c9dbab777206c95b0f8da2cf888601a22
Paulo Graça <paulo1978@gmail.com>
2021-11-26 12:32:51 +0000

    criação de handles, migração dos campos de authorprofile, e suporte de tipo de coleção

--- src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.0_2021.11.24__RCAAP_AuthorProfile_to_item.sql
+++ src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/V5.0_2021.11.24__RCAAP_AuthorProfile_to_item.sql
@@ -1,3 +1,4 @@
+-- . Adicionar o suporte do campo "relation.isAuthorOfPublication" schema e "relation.isPublicationOfAuthor"
 -- . Adicionar o suporte do campo "dspace.entity.type" schema: dspace http://dspace.org/dspace
 -- . Iremos ignorar os bitstreams do authorprofile
 -- Para cada registo de authorprofile
@@ -9,6 +10,22 @@
 
 
 -- 
+
+BEGIN;
+
+-- Criar schema para relacoes "relation.isAuthorOfPublication" e "relation.isPublicationOfAuthor"
+
+INSERT INTO "metadataschemaregistry" ( "metadata_schema_id", "namespace", "short_id") 
+VALUES (nextval('metadataschemaregistry_seq'), 'http://dspace.org/relation', 'relation' );
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'relation'), 'isAuthorOfPublication');
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'relation'), 'isPublicationOfAuthor');
+
+COMMIT;
+
 
 
 BEGIN;
@@ -54,6 +71,11 @@ INSERT INTO "resourcepolicy" ( "policy_id", "resource_type_id", "resource_id", "
 VALUES ( nextval('resourcepolicy_seq'), 4, currval('community_seq'), 0, 0 );
 
 
+-- CRIAR HANDLE
+INSERT INTO "public"."handle" ( "handle_id", "handle", "resource_type_id", "resource_id") 
+VALUES ( nextval('handle_seq'), (select substring(handle from 0 for position('/' in handle)) from handle order by handle_id DESC limit 1) || '/' || currval('handle_seq'), 4, currval('community_seq') );
+
+
 -- #### COLLECTION ######
 -- criar coleção para pessoas
 
@@ -64,6 +86,11 @@ INSERT INTO metadatavalue (resource_id, resource_type_id, metadata_field_id, tex
 values ( currval('collection_seq'), 3,
 (select metadata_field_id from metadatafieldregistry where metadata_schema_id=(select metadata_schema_id from metadataschemaregistry where short_id='dc') and element = 'title' and qualifier is null),
 'Pessoas', null, 0);
+INSERT INTO metadatavalue (resource_id, resource_type_id, metadata_field_id, text_value, text_lang, place)
+values ( currval('collection_seq'), 3,
+(select metadata_field_id from "metadatafieldregistry" WHERE metadata_schema_id = (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'dspace') and element = 'entity' and qualifier = 'type'),
+'Person', null, 0);
+
 
 -- relação coleção-comunidade
 INSERT INTO "community2collection" ( "id", "community_id", "collection_id") 
@@ -76,6 +103,9 @@ VALUES ( nextval('resourcepolicy_seq'), 3, currval('collection_seq'), 0, 0 );
 INSERT INTO "resourcepolicy" ( "policy_id", "resource_type_id", "resource_id", "action_id", "epersongroup_id") 
 VALUES ( nextval('resourcepolicy_seq'), 3, currval('collection_seq'), 10, 0 );
 
+-- CRIAR HANDLE
+INSERT INTO "public"."handle" ( "handle_id", "handle", "resource_type_id", "resource_id") 
+VALUES ( nextval('handle_seq'), (select substring(handle from 0 for position('/' in handle)) from handle order by handle_id DESC limit 1) || '/' || currval('handle_seq'), 3, currval('collection_seq') );
 
 -- #### ITEM ######
 -- criar coluna item_id no author profile
@@ -114,6 +144,14 @@ SELECT
 	item_id as "item_id"
 FROM authorprofile;
 
+-- CRIAR HANDLE
+INSERT INTO "public"."handle" ( "handle_id", "handle", "resource_type_id", "resource_id")
+SELECT
+	nextval('handle_seq') as "handle_id",
+	(select substring(handle from 0 for position('/' in handle)) from handle order by handle_id DESC limit 1) || '/' || currval('handle_seq') as "handle",
+	2 as "resource_type_id",
+	item_id as "resource_id"
+FROM authorprofile;
 
 -- #### METADATAVALUE ######
 -- mover metadata do author profile
@@ -139,6 +177,7 @@ CREATE TABLE "author_relationship" (
 	"item_id" Integer NOT NULL,
 	"metadata_field_id" Integer,
 	"text_value" Text,
+	"place" Integer,
 	PRIMARY KEY ( "author_relationship_id" ) );
  ;
 -- -------------------------------------------------------------
@@ -147,14 +186,15 @@ COMMIT;
 
 BEGIN;
 
-INSERT INTO "author_relationship" ("author_relationship_id", "author_item_id", "metadata_value_id", "item_id", "metadata_field_id", "text_value"  )
+INSERT INTO "author_relationship" ("author_relationship_id", "author_item_id", "metadata_value_id", "item_id", "metadata_field_id", "text_value", "place" )
 SELECT
 	nextval('author_relationship_seq') as "author_relationship_id", 
 	ap.item_id as "author_item_id",
 	mv.metadata_value_id as "metadata_value_id", 
 	mv.resource_id as "item_id",
 	mv.metadata_field_id as "metadata_field_id",
-	mv.text_value as "text_value"
+	mv.text_value as "text_value",
+	mv.place as "place"
 FROM metadatavalue as mv inner join authorprofile as ap on ap.uuid = authority;
 
 COMMIT;
@@ -187,4 +227,74 @@ BEGIN;
 -- DROP SEQUENCE "authorprofile_seq" ---------------------------
 DROP SEQUENCE IF EXISTS "authorprofile_seq" CASCADE;
 -- -------------------------------------------------------------
+COMMIT;
+
+
+-- -------------------------------------------------------------
+-- ---------  atualizar metadados ------------------------------
+-- -------------------------------------------------------------
+
+BEGIN;
+-- -------------------------------------------------------------
+INSERT INTO "metadataschemaregistry" ( "metadata_schema_id", "namespace", "short_id") 
+VALUES (nextval('metadataschemaregistry_seq'), 'https://schema.org/Person', 'person' );
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'familyName');
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'givenName');
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'identifier');
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element", "qualifier") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'identifier', 'ciencia-id' );
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element", "qualifier") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'identifier', 'orcid' );
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element", "qualifier") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'identifier', 'rid' );
+
+INSERT INTO "metadatafieldregistry" ( "metadata_field_id","metadata_schema_id", "element", "qualifier") 
+VALUES (nextval('metadatafieldregistry_seq'), (SELECT metadata_schema_id FROM "metadataschemaregistry" WHERE "short_id" = 'person'), 'identifier', 'scopus-author-id' );
+
+
+COMMIT;
+
+
+-- mover os field type id existentes de author profile para os do schema.org/person
+BEGIN;
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'dc') and metadatafieldregistry.element = 'title' and metadatafieldregistry.qualifier is NULL)
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'author' and metadatafieldregistry.qualifier IS NULL);
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'dc') and metadatafieldregistry.element = 'title' and metadatafieldregistry.qualifier = 'alternative')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'name' and metadatafieldregistry.qualifier = 'variant');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'dc') and metadatafieldregistry.element = 'subject' and metadatafieldregistry.qualifier = 'other')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'specialization' and metadatafieldregistry.qualifier IS NULL);
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'familyName' and metadatafieldregistry.qualifier is NULL)
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'name' and metadatafieldregistry.qualifier = 'last');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'givenName' and metadatafieldregistry.qualifier is NULL)
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'name' and metadatafieldregistry.qualifier = 'first');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'identifier' and metadatafieldregistry.qualifier = 'ciencia-id')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'id' and metadatafieldregistry.qualifier = 'cienciaID');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'identifier' and metadatafieldregistry.qualifier = 'orcid')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'id' and metadatafieldregistry.qualifier = 'orcid');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'identifier' and metadatafieldregistry.qualifier = 'rid')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'id' and metadatafieldregistry.qualifier = 'researcherID');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'identifier' and metadatafieldregistry.qualifier = 'scopus-author-id')
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'id' and metadatafieldregistry.qualifier = 'scopusAuthorID');
+
+UPDATE metadatavalue SET metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'person') and metadatafieldregistry.element = 'identifier' and metadatafieldregistry.qualifier is NULL)
+WHERE metadata_field_id = (select metadata_field_id from "metadatafieldregistry" WHERE metadatafieldregistry.metadata_schema_id = (SELECT mr.metadata_schema_id FROM "metadataschemaregistry" as mr WHERE "short_id" = 'authorProfile') and metadatafieldregistry.element = 'id' and metadatafieldregistry.qualifier = 'other');
+
 COMMIT;
