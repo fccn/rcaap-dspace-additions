77d46942d4fa7f18ba389e051ca0bd3e7d9d8f20
Paulo Graça <paulo1978@gmail.com>
2021-11-26 12:30:40 +0000

    corrigir place esquerda e direita nas relacoes

--- src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/RCAAP_create_author_relationship_DS7.sql
+++ src/main/resources/org/dspace/storage/rdbms/sqlmigration/postgres/RCAAP_create_author_relationship_DS7.sql
@@ -1,21 +1,35 @@
 -- . Executar 
 -- relation.isPublicationOfAuthor
-BEGIN;
 
+-- #########################################
+
+--            NOTA: 
+--    Antes de executar este script, deverá 
+--    ser carregado o modelo de entidades 
+--    para a base de dados, bem como garantir
+--    os registries
+
+
+-- #########################################
+
+
+
+BEGIN;
+-- Criar as relações entre itens
 INSERT INTO "relationship" ( "id","left_id", "right_id", "rightward_value", "left_place", "right_place", "type_id") 
 SELECT 
 	nextval('relationship_id_seq') as "id",
 	item.uuid as "left_id", 
 	author.uuid as "right_id", 
 	ar.text_value as "rightward_value", 
-	0 as "left_place",
+	ar.place as "left_place",
 	0 as "right_place", 
 	(SELECT id FROM "relationship_type" where leftward_type = 'isAuthorOfPublication'
 		AND left_type = (select id from entity_type where label='Publication') 
 		AND right_type = (select id from entity_type where label='Person')) as "type_id"
 FROM author_relationship AS ar
-INNER JOIN item as "item" on ar.author_item_id = item.item_id
-INNER JOIN item as "author" on ar.item_id = author.item_id;
+INNER JOIN item as "author" on ar.author_item_id = author.item_id
+INNER JOIN item as "item" on ar.item_id = item.item_id;
 
 COMMIT;
 -- apagar tabela temporaria das relações
