7b252b2d347efc3e7f9407180f6ceae7bdbec543
Fernando Ribeiro <fernando.ribeiro@fccn.pt>
2023-02-27 11:31:39 +0000

    Correct Identation

--- src/main/java/org/dspace/external/provider/impl/RenatesLiveImporter.java
+++ src/main/java/org/dspace/external/provider/impl/RenatesLiveImporter.java
@@ -1,13 +1,11 @@
 package org.dspace.external.provider.impl;
 
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Logger;
 import org.dspace.content.dto.MetadataValueDTO;
 import org.dspace.external.RenatesConnector;
 import org.dspace.external.model.ExternalDataObject;
 import org.dspace.external.provider.AbstractExternalDataProvider;
-//import org.jaxen.JaxenException;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.io.IOException;
@@ -57,18 +55,18 @@ public class RenatesLiveImporter extends AbstractExternalDataProvider {
      * @param id    The id on which will be searched - Renates
      * @return The metadata to include in the input-forms
      */
-	@Override
+    @Override
     public Optional<ExternalDataObject> getExternalDataObject(String id) {
-        
-		InputStream result = renatesConnector.getRenatesData(id, 0, 0);
+
+        InputStream result = renatesConnector.getRenatesData(id, 0, 0);
         SAXBuilder builder = new SAXBuilder();
         Document document = null;
-     
+
         try {
-			
-			document = builder.build(result);
-			
-			XPathFactory xpfac = XPathFactory.instance();
+
+            document = builder.build(result);
+
+            XPathFactory xpfac = XPathFactory.instance();
             XPathExpression<Element> elements = xpfac.compile("//Tese/Tese", Filters.element());
             List<Element> teseElements = elements.evaluate(document);
 
@@ -76,41 +74,41 @@ public class RenatesLiveImporter extends AbstractExternalDataProvider {
             //This one has the data per se if we don't use extends Abstract...
             if(teseElements.size() > 0) {
                 //There is only one record
-				Element tese = elements.evaluate(document).get(0);
+                Element tese = elements.evaluate(document).get(0);
                 return getRenatesExternalDataObject(tese).stream().findFirst();
                 //return Optional.of(getRenatesExternalDataObject(elements).stream().findFirst().get());
             }
 
         } catch (JDOMException | IOException e) {
             //log.debug(e.printStackTrace());
-			e.printStackTrace();
+            e.printStackTrace();
         }
         return Optional.empty();
     }
-	 
-    
-	@Override
+
+
+    @Override
     public List<ExternalDataObject> searchExternalDataObjects(String query, int start, int limit) {
-        
-		log.info("Quering Renates to get the Thesis identified by " + query.trim());
-        
-		query = query.trim();
+
+        log.info("Quering Renates to get the Thesis identified by " + query.trim());
+
+        query = query.trim();
         Pattern pattern = Pattern.compile(tidRegExp);
         Matcher matcher = pattern.matcher(query);
         if(!matcher.find()){
             log.debug("The search query doesn't match TID Criteria");
             throw new RuntimeException("The search query doesn't match TID Criteria");
         }
-		
-		InputStream result = renatesConnector.getRenatesData(query, 0, 0);
+
+        InputStream result = renatesConnector.getRenatesData(query, 0, 0);
         SAXBuilder builder = new SAXBuilder();
         Document document = null;
-     
+
         try {
-			
-			document = builder.build(result);
-			
-			XPathFactory xpfac = XPathFactory.instance();
+
+            document = builder.build(result);
+
+            XPathFactory xpfac = XPathFactory.instance();
             XPathExpression<Element> elements = xpfac.compile("//Tese/Tese", Filters.element());
             List<Element> teseElements = elements.evaluate(document);
 
@@ -118,19 +116,19 @@ public class RenatesLiveImporter extends AbstractExternalDataProvider {
             //This one has the data per se if we don't use extends Abstract...
             if(teseElements.size() > 0) {
                 //There is only one record
-				Element tese = elements.evaluate(document).get(0);
+                Element tese = elements.evaluate(document).get(0);
                 return getRenatesExternalDataObject(tese).stream().collect(Collectors.toList());
             }
 
         } catch (JDOMException | IOException e) {
-			//log.debug(e.printStackTrace());
-			e.printStackTrace();
+            //log.debug(e.printStackTrace());
+            e.printStackTrace();
         }
-		
+
         return Collections.emptyList();
-		
+
     }
-	
+
 
     @Override
     public boolean supports(String source) {
@@ -139,8 +137,6 @@ public class RenatesLiveImporter extends AbstractExternalDataProvider {
 
     @Override
     public int getNumberOfResults(String query) {
-        //Should return 1 if exists - But it return on maximum 1
-        //Call API RENATES...????
         return 1;
     }
 
@@ -186,73 +182,68 @@ public class RenatesLiveImporter extends AbstractExternalDataProvider {
         //can be used to get token or to initialize other things
     }
 
-	/**
+    /**
      *
      * @param elements data form DGEEC API - element tese
      * @return the list of 1 result if exists
-	 * Using to JDOM2 as Axiom is deprecated in DSpace
-    */
-	private Collection<ExternalDataObject> getRenatesExternalDataObject(Element tese){
-			
-		List<ExternalDataObject> records = new LinkedList<ExternalDataObject>();
+     * Using to JDOM2 as Axiom is deprecated in DSpace
+     */
+    private Collection<ExternalDataObject> getRenatesExternalDataObject(Element tese){
+
+        List<ExternalDataObject> records = new LinkedList<ExternalDataObject>();
         ExternalDataObject externalDataObject = new ExternalDataObject(sourceIdentifier);
-			
-		String tid = tese.getChildren("TID").get(0).getText().trim();
-		String title = tese.getChildren("TemaTese").get(0).getText().trim();
-		String author = tese.getChildren("NomeCompleto").get(0).getText().trim();
-		if(author.lastIndexOf(" ") > 0){
-			author = author.substring(author.lastIndexOf(" ")+1) + ", " + author.substring(0, author.lastIndexOf(" "));
-		}
 
-		String type = (tese.getChildren("TipoTese").get(0).getText()).equalsIgnoreCase("mestrado")  ? "master thesis" : "doctoral thesis";
-		
-		externalDataObject.addMetadata(new MetadataValueDTO("dc","identifier","tid","",tid));
-		externalDataObject.addMetadata(new MetadataValueDTO("dc","title",null, "",title));
-		externalDataObject.addMetadata(new MetadataValueDTO("dc","type",null, "", type));
-		externalDataObject.addMetadata(new MetadataValueDTO("dc","contributor","author", "", author));
-		
-		
-		try {
-			
-			String dateIssued = tese.getChildren("DataGrau").get(0).getText().trim();
-			if(dateIssued != null ){
-				StringBuilder sb = new StringBuilder();
-				java.lang.String dates[] = dateIssued.split("-");
-				if(dates.length > 2)
-					dateIssued = sb.append(dates[2]).append("-").append(dates[1]).append("-").append(dates[0]).toString();
-				else
-					dateIssued = dates.length > 1 ? sb.append(dates[1]).append("-").append(dates[0]).toString() : dateIssued;
-			}
-			
-			externalDataObject.addMetadata(new MetadataValueDTO("dc","date","issued", "", dateIssued));
-			
-			
-			String keywords = tese.getChildren("Palavras_chave").get(0).getText();
-			if (keywords != null) {
-				String[] subjects = keywords.split("[,;]");
-				if (subjects.length > 0) {
-					for (String subject : subjects) {
-						externalDataObject.addMetadata(new MetadataValueDTO("dc","subject", null,"", subject.trim()));
-					}
-				} else
-				   externalDataObject.addMetadata(new MetadataValueDTO("dc","subject",null, "", keywords.trim()));
-				}
-		}
-		catch(Exception e){
-			//Do nothing;
-		}
+        String tid = tese.getChildren("TID").get(0).getText().trim();
+        String title = tese.getChildren("TemaTese").get(0).getText().trim();
+        String author = tese.getChildren("NomeCompleto").get(0).getText().trim();
+        if(author.lastIndexOf(" ") > 0){
+            author = author.substring(author.lastIndexOf(" ")+1) + ", " + author.substring(0, author.lastIndexOf(" "));
+        }
 
-		//Other way to do the thing is to comppile for every Element...
-		//XPathExpression<Element> el = xpfac.compile("//Tese/Tese/TID", Filters.element());
-		//Element e = el.evaluate(document).get(0);
-		
-		externalDataObject.setId(tid);
-		externalDataObject.setDisplayValue(title);
-		externalDataObject.setValue(title);
+        String type = (tese.getChildren("TipoTese").get(0).getText()).equalsIgnoreCase("mestrado")  ? "master thesis" : "doctoral thesis";
 
-		records.add(externalDataObject);
-		return records;
+        externalDataObject.addMetadata(new MetadataValueDTO("dc","identifier","tid","",tid));
+        externalDataObject.addMetadata(new MetadataValueDTO("dc","title",null, "",title));
+        externalDataObject.addMetadata(new MetadataValueDTO("dc","type",null, "", type));
+        externalDataObject.addMetadata(new MetadataValueDTO("dc","contributor","author", "", author));
 
-    }   
+
+        try {
+
+            String dateIssued = tese.getChildren("DataGrau").get(0).getText().trim();
+            if(dateIssued != null ){
+                StringBuilder sb = new StringBuilder();
+                java.lang.String dates[] = dateIssued.split("-");
+                if(dates.length > 2)
+                    dateIssued = sb.append(dates[2]).append("-").append(dates[1]).append("-").append(dates[0]).toString();
+                else
+                    dateIssued = dates.length > 1 ? sb.append(dates[1]).append("-").append(dates[0]).toString() : dateIssued;
+            }
+
+            externalDataObject.addMetadata(new MetadataValueDTO("dc","date","issued", "", dateIssued));
+
+            String keywords = tese.getChildren("Palavras_chave").get(0).getText();
+            if (keywords != null) {
+                String[] subjects = keywords.split("[,;]");
+                if (subjects.length > 0) {
+                    for (String subject : subjects) {
+                        externalDataObject.addMetadata(new MetadataValueDTO("dc","subject", null,"", subject.trim()));
+                    }
+                } else
+                    externalDataObject.addMetadata(new MetadataValueDTO("dc","subject",null, "", keywords.trim()));
+            }
+        }
+        catch(Exception e){
+            //Do nothing;
+        }
+
+        externalDataObject.setId(tid);
+        externalDataObject.setDisplayValue(title);
+        externalDataObject.setValue(title);
+
+        records.add(externalDataObject);
+        return records;
+
+    }
 
 }
